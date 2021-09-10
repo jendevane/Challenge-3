7af9837381d0ef0978225e9dcf91605228cfce1f
jendevane <87095302+jendevane@users.noreply.github.com>
2021-09-09 23:29:16 -0400

    add

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,61 @@
-// Assignment code here
+// prompt variables
+let paswLength= 0
+let useChars = false
+let useSymbols = false
+let useNumbers = false
+// pw types
+let lowerChars = 'abcdefghijklmnopqrstuvwxyz'
+let upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let symbols = '!@#$%^&*'
+let numbers = '1234567890'
+
+// function promptMe(){
+  document.getElementById("generate").onclick = function()
+{
+  paswLength = prompt("Enter Password length (8 - 128) characters",8);
+  while (paswLength < 8 || paswLength >128) {
+     
+  }
+  }
+let result=confirm("Do you want lower case charaters?");
+ 
+
+
+
+
+
+
+// final pw string
+let charset = ''
+
+function generatePassword() {
+  var length = paswLength
+  if (lowerChars === true) {
+    charset = lowerChars
+  }
+  if (upperChars === true) {
+    charset = upperChars
+  }
+  if (useChars === true && useNumbers === true) {
+    charset = chars + numbers
+  }
+  if (useChars === true && useNumbers === true && useSymbols === true) {
+    charset = chars + numbers + symbols
+  }
+  console.log(charset)
+  retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+let submit = documentGetElementById('generate')
+
+// fake code below
+submit.click(generatePassword())
+
+
 
 
 // Get references to the #generate element
